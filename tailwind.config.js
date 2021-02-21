bbd29ad0ee0791249652f7af6546e19f1f691fad
unknown <mctathul@gmail.com>
2021-02-21 12:06:30 +0400

    installed tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: [
+      './resources/views/**/*.blade.php',
+    ]
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+ 
