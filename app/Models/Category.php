5e275af70b7bd4f2371005ddc8b8c9f2c9958dc9
unknown <mctathul@gmail.com>
2021-03-01 15:22:42 +0400

    category created with post pivot table

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,13 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model
+class Category extends Model
 {
     use HasFactory;
 
+    public function posts(){
 
-    public function categories(){
-
-        return $this->belongsToMany(Category::class);
+        return $this->belongsToMany(Post::class);
     }
 }
